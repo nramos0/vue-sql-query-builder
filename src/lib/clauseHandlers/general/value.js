07d9f1910a8493e520c0895a008d5a38aa7b94e8
nramos0 <xoen000@163.com>
2021-08-30 02:21:01 +0800

    add optional name for value input

--- src/lib/clauseHandlers/general/value.js
+++ src/lib/clauseHandlers/general/value.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 import { generateInputChild, getASTValue, assignAST } from "../../util";
 
-export const value = (value, component, children, nest) => {
+export const value = (value, component, children, nest, name = null) => {
   children.push(
     generateInputChild({
       onChange: (e) => {
         assignAST(value, getASTValue(e.target.value));
-        console.log(`clause value nest ${nest} updated`);
+        console.log(`${name ? `${name}` : "value"} nest ${nest} updated`);
       },
     })
   );
