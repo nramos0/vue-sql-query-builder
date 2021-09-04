c575467377c7e6a06824e456a000a02fba7629c5
realdoonser <pwyaywriting@gmail.com>
2021-09-04 06:57:39 -0500

    fixed bug in groupby

--- src/lib/clauseHandlers/select/groupby.js
+++ src/lib/clauseHandlers/select/groupby.js
@@ -1,7 +1,19 @@
-import { generateSpanChild } from "../../util";
-import { value } from "../general";
+import {
+    generateSpanChild,
+    generateInputChild,
+    setAST_GroupBy,
+} from "../../util";
+//import { value } from "../general";
 
 export const selectGroupby = (having, component, children, nest) => {
-  children.push(generateSpanChild("GROUP BY"));
-  value(having, component, children, nest);
+    children.push(generateSpanChild("GROUP BY"));
+    // value(having, component, children, nest);
+    children.push(
+        generateInputChild({
+            onChange: (e) => {
+                setAST_GroupBy(having, e.textInput);
+                console.log(`clause value nest ${nest} updated`);
+            },
+        })
+    );
 };
